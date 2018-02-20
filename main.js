bba4cced3f70aa07faf3baacae4f5cf68f1a0687
Saad Khan <saad.khan@dawsoncollege.qc.ca>
2018-02-20 15:44:28 -0500

    internet ie8

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict"
+function createDummyElements() {
+  var semanticElements = [
+    "article", "aside", "details", "figcaption", "figure",
+    "footer", "header", "hgroup", "menu", "nav", "section"
+  ];
+  for (var i = 0; i < semanticElements.length; i++) {
+    document.createElement(semanticElements[i]);
+  }
+}
+createDummyElements();
